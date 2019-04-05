e1dc9d697294d0e29c222c10389c5e2ece0641c0
MartinBerthold <MartinBerthold@NB-FC-215335>
2019-04-05 13:09:46 +0200

    add tag

--- src/q2g-con-hypercube-qvx/TableServer.cs
+++ src/q2g-con-hypercube-qvx/TableServer.cs
@@ -200,6 +200,8 @@ namespace q2gconhypercubeqvx
         {
             try
             {
+                Thread.Sleep(10000);
+
                 QvDataContractResponse response;
                 var parameter = ConnectorParameter.Create(connection?.MParameters);
                 logger.Trace($"HandleJsonRequest {method}");
